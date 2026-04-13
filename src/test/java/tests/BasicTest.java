ac20f675c203637873611a79969f5f61738fd1ee
Adam McWhirter <adammcwhirterwork@gmail.com>
2026-04-13 11:56:11 +0100

    Initial commit

---
+++ src/test/java/tests/BasicTest.java
@@ -0,0 +1,26 @@
+package tests;
+
+import org.junit.jupiter.api.*;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class BasicTest {
+
+    WebDriver driver;
+
+    @BeforeEach
+    void setup() {
+        driver = new ChromeDriver();
+    }
+
+    @Test
+    void openWebsite() {
+        driver.get("https://automationexercise.com");
+        Assertions.assertTrue(driver.getTitle().contains("Automation"));
+    }
+
+    @AfterEach
+    void teardown() {
+        driver.quit();
+    }
+}
